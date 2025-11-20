4523f076665ec617312d5cb12a49db6e1a9806c1
ridhocrnv <ridhoahmadirawan281@gmail.com>
2025-11-20 12:01:27 +0800

    "test"

---
+++ script/data/songs.js
@@ -0,0 +1,105 @@
+// --- Data Playlist ---
+const songs = [
+    {
+        title: 'Aku dan Dirimu (feat. Ari Lasso)',
+        artist: 'Bunga Citra Lestari, Ari Lasso',
+        src: 'src/aku-dan-dirimu.mp3',
+        lyrics: [
+            { time: 0, text: "..." },
+            { time: 0.3, text: "Duhai cintaku, sayangku, lepaskanlah" },
+            { time: 8.0, text: "Perasaanmu, rindumu, seluruh cintamu" },
+            { time: 16.0, text: "Dan kini hanya ada aku dan dirimu" },
+            { time: 24.5, text: "Sesaat di keabadian" },
+            { time: 30.0, text: "♫" },
+        ]
+    },
+    {
+        title: 'Til Death Do Us Part',
+        artist: 'White Lion',
+        src: 'src/til-death-do-us-part.mp3',
+        lyrics: [
+            {time: 0, text: "In this world"},
+            {time: 2.5, text: "That I know I wouldn't do"},
+            {time: 6.5, text: "To be near you every day"},
+            {time: 10.5, text: "Every hour, every minute"},
+            {time: 17.0, text: "Take my hand"},
+            {time: 18.5, text: "And let me lead the way"},
+            {time: 24.5, text: "All through your life"},
+            {time: 28.5, text: "I'll be by your side"},
+            {time: 32.5, text: "'Til death do us part"},
+            {time: 40.0, text: "Baby, I'll be your friend"},
+            {time: 44.0, text: "My love will never end"},
+            {time: 47.5, text: "'Til death do us part"},
+            {time: 55.0, text: "When I wake up every day"},
+        ] // Kosongkan array jika belum sempat isi lirik
+    },
+    {
+        title: "You're All I Need",
+        artist: 'White Lion',
+        src: 'src/youre-all-i-need.mp3',
+        lyrics: [
+            {time: 0, text: "..."},
+            {time: 1.0, text: "She doesn't know"},
+            {time: 3.0, text: "You're all I need"},
+            {time: 5.0, text: "Beside me, girl!"},
+            {time: 7.5, text: "You're all I need"},
+            {time: 9.0, text: "To turn my world"},
+            {time: 11.5, text: "You're all I want"},
+            {time: 13.0, text: "Inside my heart"},
+            {time: 15.0, text: "You're all I need"},
+            {time: 17.0, text: "When we're apart"},
+            {time: 22.5, text: "Say"},
+            {time: 24.5, text: "Say that you'll be there"},
+            {time: 29.5, text: "Whenever I reach out"},
+            {time: 32.5, text: "To feel your hand in mine"},
+            {time: 38.5, text: "Stay"},
+            {time: 40.5, text: "Stay within my heart"},
+            {time: 45.5, text: "Whenever I'm alone"},
+            {time: 48.5, text: "I know that you are there"},
+            {time: 53.5, text: "Oooooh!"},
+            {time: 57.5, text: "♫"},
+        ] // Kosongkan array jika belum sempat isi lirik
+    },
+    {
+        title: "I Live My Life for You",
+        artist: 'Firehouse',
+        src: 'src/i-live-my-life-for-you.mp3',
+        lyrics: [
+            {time: 0, text: "For you"},
+            {time: 2.0, text: "I want to be by your side"},
+            {time: 5.5, text: "In everything that you do"},
+            {time: 8.5, text: "And if there's only one thing"},
+            {time: 11.5, text: "You can believe is true"},
+            {time: 18.5, text: "I live my life for you"},
+            {time: 30.5, text: "I dedicate my life to you"},
+            {time: 34.0, text: "You know that I would die"},
+            {time: 36.0, text: "For you"},
+            {time: 37.5, text: "But our love would last"},
+            {time: 40.5, text: "Forever"},
+            {time: 44.5, text: "And I will always be with you"},
+            {time: 47.5, text: "And there is nothing"},
+            {time: 49.0, text: "We can't do"},
+            {time: 51.5, text: "As long as we're together"},
+            {time: 58.5, text: "I just can't live without you"},
+        ] // Kosongkan array jika belum sempat isi lirik
+    },
+    {
+        title: "When I Look Into Your Eyes",
+        artist: 'Firehouse',
+        src: 'src/when-i-look-into-your-eyes.mp3',
+        lyrics: [
+            {time: 0, text: "'Cause I see my whole world"},
+            {time: 3.0, text: "I see only you"},
+            {time: 6.5, text: "When I look into your eyes"},
+            {time: 13.0, text: "I can see how much I love you"},
+            {time: 15.7, text: "And it makes me realize"},
+            {time: 19.4, text: "When I look into your eyes"},
+            {time: 25.8, text: "I see all my dreams come true"},
+            {time: 29.4, text: "When I look into your eyes"},
+            {time: 33.5, text: "♫"},
+        ] // Kosongkan array jika belum sempat isi lirik
+    },
+    // ... tambahkan lagu lain di sini ...
+];
+
+export default songs;
